78c9404d2e4b5c0eccf1eab9f147b80d4b4469d1
Alessandro dos santos pinto <lastripper@gmail.com>
2015-03-10 21:43:45 -0300

    first commit

---
+++ ADColorImage/UIImage+ADColorImage.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+ADColorImage.h
+//  ADColorImage
+//
+//  Created by Alessandro dos santos pinto on 3/10/15.
+//  Copyright (c) 2015 Alessandro dos santos pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (ADColorImage)
+
+@end
---
+++ ADColorImage/UIImage+ADColorImage.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+ADColorImage.m
+//  ADColorImage
+//
+//  Created by Alessandro dos santos pinto on 3/10/15.
+//  Copyright (c) 2015 Alessandro dos santos pinto. All rights reserved.
+//
+
+#import "UIImage+ADColorImage.h"
+
+@implementation UIImage (ADColorImage)
+
+@end
